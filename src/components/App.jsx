788a7612e9c669a15722ed664863a914b3b4e5fb
arthurshelbaev <konodiobrandoda@mail.ru>
2022-12-13 02:46:16 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import Buttons from './Buttons'
 import Number from './Number'
 import "../styles.css"
-import { createStore } from 'redux'
 
 const App = () => {
 
-    const reducer = (state, action) => {
+    const initialState = {
+        number: 0,
+        theme: "dark"
+    }
 
+    const reducer = (state = initialState, action) => {
+        switch (action.type) {
+            case "plus":
+                return {
+                    ...state, number: state.number + 1
+                }
+            case "minus":
+                if (state.number > 0) {
+                return {
+                    ...state, number: state.number - 1
+                }}
+            case "break":
+                return {
+                    ...state, number: state.number = 0
+                }
+                break;
+        
+            default:
+                return state;
+        }
     }
 
     const store = createStore(reducer)
@@ -17,8 +40,8 @@ const App = () => {
         <div className='body'>
             <Provider store={store}>
                 <Number />
+                <Buttons />
             </Provider>
-            <Buttons />
         </div>
     )
 }
