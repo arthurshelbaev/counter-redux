e8c6ff52e04226168d5becca4b4d871d38c09aa6
arthurshelbaev <konodiobrandoda@mail.ru>
2022-12-13 02:46:34 +0300

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import "../styles.css"
 
 
 const Buttons = () => {
+
+    const dispatch = useDispatch()
+
+    const handleInc = () => {
+        dispatch({type: "plus"})
+    }
+
+    const handleDec =() => {
+        dispatch({ type: "minus" })
+    }
+
     return (
         <div className='buttons'>
-            <button className='btn1'>-</button>
-            <button className='btn2'>+</button>
+            <button className='btn1' onClick={handleDec}>–</button>
+            <button className='btn2'onClick={handleInc}>+</button>
         </div>
     );
 };
