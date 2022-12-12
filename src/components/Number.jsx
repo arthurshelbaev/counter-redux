2b60b3f07380242b1f420ef97364a3c8376a0aed
arthurshelbaev <konodiobrandoda@mail.ru>
2022-12-13 02:47:39 +0300

    Update Number.jsx

--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Number = () => {
+
+    const dispatch = useDispatch()
+
+    const breeak = () => {
+        dispatch({type: "break"})
+    }
+
+    const number = useSelector((state) => state.number)
+    const theme = useSelector((state) => state.theme)
+    
     return (
-        <div className='number'>1</div>
+        <div className='number' onClick={breeak}>{number}</div>
     );
 };
 
