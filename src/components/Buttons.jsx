31c58ad879b15119da382eba1142783efe709bff
arthurshelbaev <konodiobrandoda@mail.ru>
2022-12-13 02:48:02 +0300

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import "../styles.css"
 
-const Number = () => {
+
+const Buttons = () => {
 
     const dispatch = useDispatch()
 
-    const breeak = () => {
-        dispatch({type: "break"})
+    const handleInc = () => {
+        dispatch({type: "plus"})
     }
 
-    const number = useSelector((state) => state.number)
-    const theme = useSelector((state) => state.theme)
-    
+    const handleDec =() => {
+        dispatch({ type: "minus" })
+    }
+
     return (
-        <div className='number' onClick={breeak}>{number}</div>
+        <div className='buttons'>
+            <button className='btn1' onClick={handleDec}>–</button>
+            <button className='btn2'onClick={handleInc}>+</button>
+        </div>
     );
 };
 
-export default Number;
+export default Buttons;
